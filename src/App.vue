2f0bcef6f5e833a819606045431beb1a73836ec9
kos3l <71730597+kos3l@users.noreply.github.com>
2021-09-28 19:36:23 +0200

    building navigation bar desktop

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,20 @@
 <template>
   <v-app id="app">
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
+      <v-container fluid class="secondary d-none d-md-block nav pa-0">
+        <v-row class="height-fix">
+          <v-col
+            class="height-fix pa-0 pt-3 d-flex justify-center align-center"
+          >
+            <p class="ma-0">ASS</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="nav-box d-flex justify-center"> PAINTING </v-col>
+          <v-col class="nav-box d-flex justify-center"> DRAWING </v-col>
+          <v-col class="nav-box d-flex justify-center"> BUNDLES </v-col>
+        </v-row>
+      </v-container>
       <router-view />
     </v-main>
   </v-app>
@@ -50,4 +31,38 @@ export default {
 </script>
 <style lang="scss">
 @import "/src/sass/variables";
+#app {
+  background-color: map-get($colors, primary);
+  font-family: "Raleway", sans-serif;
+  letter-spacing: 0.1rem;
+}
+.nav {
+  height: 3rem;
+  font-weight: bold;
+}
+.nav p {
+  color: map-get($colors, primary);
+  font-style: italic;
+}
+.height-fix {
+  height: 100%;
+}
+.nav-box {
+  @include container_mixin(
+    2px,
+    map-get($colors, secondary),
+    2rem,
+    2rem,
+    map-get($colors, secondary),
+    map-get($colors, primary)
+  );
+}
+.nav-box:first-child {
+  margin-left: 2px;
+  border-right: 0;
+}
+.nav-box:last-child {
+  margin-right: 2px;
+  border-left: 0;
+}
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,15 +7,9 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: "#333333",
-        secondary: "#2b2b2b",
-        white: "#ffffff",
-        orange: "#fc6d2b",
-        darkgrey: "#707070",
-        complete: "#2bfc83",
-        inprogress: "#ffb936",
-        incomplete: "#ff1212",
-        lightgrey: "#cacaca",
+        primary: "#EAEAF2",
+        secondary: "#04041A",
+        highlight: "EB188A",
       },
     },
   },
